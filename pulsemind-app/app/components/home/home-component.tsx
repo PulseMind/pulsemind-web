051a5102f273533605d45a8a8c4f3ce5800386bc
PulseMind <domin.wild@proton.me>
2024-11-28 12:04:41 +0100

    Logo padding and size

--- pulsemind-app/app/components/home/home-component.tsx
+++ pulsemind-app/app/components/home/home-component.tsx
@@ -6,7 +6,9 @@ export default function HomeComponent() {
   return (
     <div className="flex bg-slate-200 min-h-[100vh] items-center place-content-center shadow-[inset_0_0_80px_20px_rgba(0,0,0,0.45)]">
       <Card className="flex items-center place-content-center pr-[50px]">
-        <Logo size={250} />
+        <div className="px-12 py-10">
+          <Logo size={185} />
+        </div>
         <div>
           <h1 className="text-[#0d7dbb] text-2xl font-bold">PULSEMIND</h1>
           <p className="text-[#0097b2] font-light opacity-50">
